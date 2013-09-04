0223318e82f9b8b68ef1753761b1c2dbbbe14ab2
Bobboteck <bobboteck@MobobVirtual.(none)>
2013-09-04 13:03:36 +0200

    Aggiunta cartella contenente il codice del progetto

---
+++ Code/rSensorUS.X/rSensorUS.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   rSensorUS.c
+ * Author: bobboteck
+ *
+ * Created on 4 settembre 2013, 12.58
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
